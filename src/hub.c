be56db31c37c063475d3ae4da440621a98fc6d11
stephan-biomedical-engineer <stephanbrrs8@gmail.com>
2026-01-06 22:26:43 -0300

    SPI validated

--- src/hub.c
+++ src/hub.c
@@ -1,10 +1,8 @@
 #include <zephyr/drivers/spi.h>
-// #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 #include <string.h>
 #include "utl_io.h"
 #include "hub.h"
-
-// LOG_MODULE_REGISTER(hub, LOG_LEVEL_INF);   
 
 K_THREAD_DEFINE(hub_thread_data, HUB_THREAD_STACK_SIZE,
                     hub_thread_entry,
@@ -12,10 +10,12 @@ K_THREAD_DEFINE(hub_thread_data, HUB_THREAD_STACK_SIZE,
                     HUB_THREAD_PRIORITY, 0, 0);
 
 static const struct device *spi_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
+static const struct gpio_dt_spec ready_pin = GPIO_DT_SPEC_GET(DT_ALIAS(spi_ready), gpios);
 
 K_MSGQ_DEFINE(hub_cmd_q, sizeof(cmd_ids_t), 10, 4);
 
 #define HUB_BUFFER_SIZE 300 
+#define SPI_PACKET_SIZE 64
 static uint8_t rx_buffer[HUB_BUFFER_SIZE];
 static uint8_t tx_buffer[HUB_BUFFER_SIZE];
 
@@ -30,15 +30,15 @@ static cmd_status_payload_t status_cache;
 
 int hub_init(void)
 {
-    if (!device_is_ready(spi_dev)) 
-    {
-        return -1;
-    }
+    if (!device_is_ready(spi_dev)) return -1;
 
     memset(tx_buffer, 0, sizeof(tx_buffer));
     memset(rx_buffer, 0, sizeof(rx_buffer));
     memset(&status_cache, 0, sizeof(status_cache));
 
+    if (device_is_ready(ready_pin.port)) {
+        gpio_pin_configure_dt(&ready_pin, GPIO_OUTPUT_INACTIVE);
+    }
     return 0;
 }
 
@@ -52,70 +52,63 @@ void hub_set_status(uint8_t state, uint32_t volume, uint32_t pressure, uint8_t a
 
 int hub_get_command(cmd_ids_t *cmd) 
 {
-    int message = k_msgq_get(&hub_cmd_q, cmd, K_NO_WAIT);
-    return message;
+    return k_msgq_get(&hub_cmd_q, cmd, K_NO_WAIT);
 }
 
 void hub_thread_entry(void *p1, void *p2, void *p3) 
 {
-    struct spi_buf header_buf = 
-    { 
-        .buf = rx_buffer, 
-        .len = CMD_HDR_SIZE 
-    };
-
-    struct spi_buf_set header_set = 
-    { 
-        .buffers = &header_buf, 
-        .count = 1 
-    };
-
     while(1)
     {
-        int ret = spi_transceive(spi_dev, &spi_cfg, NULL, &header_set);
-        if (ret < 0) {
-            continue; 
-        }
+        struct spi_buf rx_buf = { .buf = rx_buffer, .len = SPI_PACKET_SIZE };
+        struct spi_buf_set rx_set = { .buffers = &rx_buf, .count = 1 };
+
+        gpio_pin_set_dt(&ready_pin, 1);
+        int ret = spi_transceive(spi_dev, &spi_cfg, NULL, &rx_set);
+        gpio_pin_set_dt(&ready_pin, 0);
+
+        if (ret < 0) continue; 
 
         uint16_t payload_size = utl_io_get16_fl(&rx_buffer[3]); 
         if (payload_size > CMD_MAX_DATA_SIZE) payload_size = CMD_MAX_DATA_SIZE;
-        
-        size_t rest_len = payload_size + CMD_TRAILER_SIZE; 
-        struct spi_buf payload_buf = { .buf = &rx_buffer[CMD_HDR_SIZE], .len = rest_len };
-        struct spi_buf_set payload_set = { .buffers = &payload_buf, .count = 1 };
-        
-        ret = spi_transceive(spi_dev, &spi_cfg, NULL, &payload_set);
-        if (ret < 0) {
-             continue; 
-        }
 
+        size_t total_valid_len = CMD_HDR_SIZE + payload_size + CMD_TRAILER_SIZE;
         uint8_t src, dst;
         cmd_ids_t req_id, res_id;
         cmd_cmds_t req_data, res_data;
-        size_t total_len = CMD_HDR_SIZE + rest_len;
         size_t tx_len = 0;
 
-        bool decode_success = cmd_decode(rx_buffer, total_len, &src, &dst, &req_id, &req_data);
+        memset(&res_data, 0, sizeof(res_data));
+
+        bool decode_success = cmd_decode(rx_buffer, total_valid_len, &src, &dst, &req_id, &req_data);
 
         if(decode_success)
         {            
-            switch(req_id) {
+            switch(req_id) 
+            {
                 case CMD_GET_STATUS_REQ_ID:
                     res_id = CMD_GET_STATUS_RES_ID;
                     res_data.status_res.status_data = status_cache;
                     break;
+
                 case CMD_VERSION_REQ_ID:
                     res_id = CMD_VERSION_RES_ID;
                     res_data.version_res.major = 1;
                     res_data.version_res.minor = 2;
                     res_data.version_res.patch = 3;
                     break;
+
+                case CMD_SET_CONFIG_REQ_ID:
+                    res_id = CMD_SET_CONFIG_RES_ID;
+                    res_data.config_res.status = CMD_OK;
+                    break;
+
                 default:
                     res_id = CMD_ACTION_RES_ID;
                     res_data.action_res.status = CMD_OK;
                     res_data.action_res.cmd_req_id = req_id;
                     
-                    if (req_id >= CMD_ACTION_RUN_REQ_ID && req_id <= CMD_ACTION_BOLUS_REQ_ID) { 
+                    if (req_id >= CMD_ACTION_RUN_REQ_ID && req_id <= CMD_ACTION_BOLUS_REQ_ID) 
+                    { 
                         k_msgq_put(&hub_cmd_q, &req_id, K_NO_WAIT);
                     }
                     break;
@@ -125,37 +118,22 @@ void hub_thread_entry(void *p1, void *p2, void *p3)
         else 
         {
             res_id = CMD_GET_STATUS_RES_ID;
-            memset(&res_data, 0, sizeof(res_data));
             uint8_t err_src = ADDR_SLAVE;
             uint8_t err_dst = ADDR_MASTER;
+            res_data.status_res.status_data = status_cache;
             cmd_encode(tx_buffer, &tx_len, &err_dst, &err_src, &res_id, &res_data);
         }
 
-        size_t transaction_len = 60; 
-
-        if (transaction_len > sizeof(tx_buffer)) 
+        if (SPI_PACKET_SIZE > tx_len) 
         {
-            transaction_len = sizeof(tx_buffer);
+            memset(&tx_buffer[tx_len], 0, SPI_PACKET_SIZE - tx_len);
         }
 
-        if (transaction_len > tx_len) 
-        {
-            memset(&tx_buffer[tx_len], 0, transaction_len - tx_len);
-        }
-
-        struct spi_buf tx_buf_s = 
-        { 
-            .buf = tx_buffer, 
-            .len = transaction_len  
-        };
+        struct spi_buf tx_buf_s = { .buf = tx_buffer, .len = SPI_PACKET_SIZE };
+        struct spi_buf_set tx_set = { .buffers = &tx_buf_s, .count = 1 };
         
-        struct spi_buf_set tx_set = 
-        { 
-            .buffers = &tx_buf_s, 
-            .count = 1 
-        };
-        
+        gpio_pin_set_dt(&ready_pin, 1);
         spi_transceive(spi_dev, &spi_cfg, &tx_set, NULL);
+        gpio_pin_set_dt(&ready_pin, 0);
     }
-    
 }
